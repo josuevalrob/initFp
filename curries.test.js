48dacf6282fa1ed3f3a6997d1336ba44d5c12228
josue.valencia <josue.valencia@check24.de>
2021-04-19 16:45:42 +0200

    get odds

--- curries.test.js
+++ curries.test.js
@@ -1,9 +1,19 @@
+const curry = f =>
+    x => y => f(x, y);
 
-const getOdds = (arr) => arr;
+
+const modulo = curry((x, y) => y % x);
+
+const isOdd = modulo(2);
+
+const filter = curry((f, arr) => arr.filter(f));
+
+const getOdds = filter(isOdd);
 
 
 test("it should get odds from an array list", ()=>{
     expect(
         getOdds([1,2,3,4,5])
-    ).toEqual([2,4])
+    ).toEqual([1,3,5])
 })
+
